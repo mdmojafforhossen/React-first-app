6c02c5958e27b7352ea24c9e8caab3b14ba81a9f
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-22 13:25:53 +0600

    New Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 // import HOOK_USESTATE from './components/HOOK_USESTATE/index';
 // import UPDATE_STATE from './components/UPDATE_STATE/Index';
 // import STOP_PROPAGATION from './components/STOP_PROPAGATION/Index'
-import EVENT_HANDLING from './components/EVENT_HANDLING/Index'
+// import EVENT_HANDLING from './components/EVENT_HANDLING/Index'
+import State from './components/STATE/State'
 
 export default function App() {
   return (
     <div>
-      < EVENT_HANDLING/>
+      < State/>
     </div>
   )
 }
