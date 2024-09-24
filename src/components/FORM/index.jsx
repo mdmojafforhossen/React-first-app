2be5f5990a9e29103e97e6ce425bda8c16ab41d4
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-25 03:59:39 +0600

    add some data

--- src/components/FORM/index.jsx
+++ src/components/FORM/index.jsx
@@ -3,43 +3,35 @@ import style from './form.module.css'
 
 export default function FORM() {
 
-  const [name,setName]=useState('');
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
+  const [user, setUser] = useState({name:"", email:"", password:""})
+  const {name, email, password}=user;
 
-  const handleNameChange =(e)=>{
-    setName(e.target.value);
+  const handleChange =(e)=>{
+    setUser({...user,[e.target.name]:e.target.value})
   }
-  const handleEmailChange =(e)=>{
-    setEmail(e.target.value);
-  }
-  const handlePasswordChange =(e)=>{
-    setPassword(e.target.value);
-  }
-  const handleSubmit =(e)=>{
-    const userInfo ={
-      name,
-      email,
-      password
-    }
-    console.log(userInfo);
-    e.preventDefault();
-  }
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  console.log(user);
+  setUser({ name: '', email: '',password:'' });
+}
+
   return (
     <div className={style.container}>
       <h1>Registration</h1>
       <form action="" onSubmit={handleSubmit}>
+
         <div className={style.formGroup}>
         <label name="Name">Full Name :</label><br/>
-        <input type="text" onChange={handleNameChange} value={name} name="name" placeholder='Enter your name' required />
+        <input type="text" onChange={handleChange} value={name} name="name" placeholder='Enter your name' required />
         </div>
         <div className={style.formGroup}>
         <label name="Email">Email :</label><br/>
-        <input type="email" onChange={handleEmailChange} value={email} name="email" placeholder='Enter your email' required />
+        <input type="email" onChange={handleChange} value={email} name="email" placeholder='Enter your email' required />
         </div>
         <div className={style.formGroup}>
         <label name="Password">Password :</label><br/>
-        <input type="password" onChange={handlePasswordChange} name="password" value={password} placeholder='Enter your password' required />
+        <input type="password" onChange={handleChange} name="password" value={password} placeholder='Enter your password' required />
         </div>
         <div className={style.formGroup}>
         <button type='submit'>Submit</button>
